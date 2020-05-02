3e7a9d349a392f9323035f492b2a8349619e76b1
JessikaFreire <37316861+JessikaFreire@users.noreply.github.com>
2020-05-01 22:37:01 -0300

    Novas páginas adicionadas

--- rating.js
+++ rating.js
@@ -2,3 +2,44 @@
 $(document).ready(function() {
   $('#rateMe1').mdbRate();
 });
+
+$('.starrating label i.fa').on('click mouseover',function(){
+    // remove classe ativa de todas as estrelas
+    $('.vote label i.fa').removeClass('active');
+    // pegar o valor do input da estrela clicada
+    var val = $(this).prev('input').val();
+    //percorrer todas as estrelas
+    $('.starrating label i.fa').each(function(){
+        /* checar de o valor clicado é menor ou igual do input atual
+        *  se sim, adicionar classe active
+        */
+        var $input = $(this).prev('input');
+        if($input.val() <= val){
+            $(this).addClass('active');
+        }
+    });
+    $("#voto").html(val); // somente para teste
+});
+//Ao sair da div vote
+$('.starrating').mouseleave(function(){
+    //pegar o valor clicado
+    var val = $(this).find('input:checked').val();
+    //se nenhum foi clicado remover classe de todos
+    if(val == undefined ){
+        $('.starrating label i.fa').removeClass('active');
+    } else { 
+        //percorrer todas as estrelas
+        $('.starrating label i.fa').each(function(){
+            /* Testar o input atual do laço com o valor clicado
+            *  se maior, remover classe, senão adicionar classe
+            */
+            var $input = $(this).prev('input');
+            if($input.val() > val){
+                $(this).removeClass('active');
+            } else {
+                $(this).addClass('active');
+            }
+        });
+    }
+    $("#voto").html(val); // somente para teste
+});
